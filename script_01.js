5f463ae1b18764aaa68f3a2a5d1520b99a052edd
Misha <host.misha@googlemail.com>
2021-07-29 14:10:25 +0200

    Fallunterschied

--- script_01.js
+++ script_01.js
@@ -1,39 +1,56 @@
-// Entscheidungsstrukturen | control structures
+// // Entscheidungsstrukturen | control structures
 
-//Deklaration
-let ageJohn, ageMark;
-let isJohnOlder, isJohnEqual;
+// //Deklaration
+// let ageJohn, ageMark;
+// let isJohnOlder, isJohnEqual;
 
-// Assigment (Wertzuweisung)
-ageJohn = 35;
-ageMark = 30;
+// // Assigment (Wertzuweisung)
+// ageJohn = 35;
+// ageMark = 30;
 
-// Test | Logisch Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
-
-
-// Ausgabe
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
+// // Test | Logisch Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
 
-/******** IF ********/
-// TINA --> There is no alternative!
-//entweeder JA oder nix ... alternativlos
+// // Ausgabe
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
 
-// if (condition)
-// if (true)
-// if (false)
-if(isJohnOlder) //(ageJohn > ageMark) geht auch
 
-{   //Ja-Zweig / true
-    console.log("John ist älter");
+// /******** IF ********/
+// // TINA --> There is no alternative!
+// //entweeder JA oder nix ... alternativlos
+
+// // if (condition)
+// // if (true)
+// // if (false)
+// if(isJohnOlder) //(ageJohn > ageMark) geht auch
+
+// {   //Ja-Zweig / true
+//     console.log("John ist älter");
+// }
+// else {
+//     // Nein -Zweig /false
+//     console.log("John ist jünger. ");
+// }
+
+// //******* Ternäre (ternary) schreibweise *******//
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+/******* Fallunterscheidung / CASE|SWITCH 1 ******/
+
+let firstName = "Jane";
+let job;
+job ="driver"; // ... fährt Taxi
+
+switch (key)
+{
+    case value:
+
+    break;
+
+    default:
+        break;
 }
-else {
-    // Nein -Zweig /false
-    console.log("John ist jünger. ");
-}
-
-//******* Ternäre (ternary) schreibweise *******//
-console.log( (isJohnOlder) ? "John is older" : "Mark is older");
