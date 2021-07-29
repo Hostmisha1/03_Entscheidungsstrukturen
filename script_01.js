a39ba1948aa9594fddbb2025db9456197a1063ac
Misha <host.misha@googlemail.com>
2021-07-29 14:43:04 +0200

    SWITCH 2

--- script_01.js
+++ script_01.js
@@ -1,74 +1,90 @@
-// // Entscheidungsstrukturen | control structures
+// // // Entscheidungsstrukturen | control structures
 
-// //Deklaration
-// let ageJohn, ageMark;
-// let isJohnOlder, isJohnEqual;
+// // //Deklaration
+// // let ageJohn, ageMark;
+// // let isJohnOlder, isJohnEqual;
 
-// // Assigment (Wertzuweisung)
-// ageJohn = 35;
-// ageMark = 30;
+// // // Assigment (Wertzuweisung)
+// // ageJohn = 35;
+// // ageMark = 30;
 
-// // Test | Logisch Aussage
-// isJohnOlder = (ageJohn > ageMark);
-// isJohnEqual = (ageJohn == ageMark);
-
-
-// // Ausgabe
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
+// // // Test | Logisch Aussage
+// // isJohnOlder = (ageJohn > ageMark);
+// // isJohnEqual = (ageJohn == ageMark);
 
 
-// /******** IF ********/
-// // TINA --> There is no alternative!
-// //entweeder JA oder nix ... alternativlos
+// // // Ausgabe
+// // console.log("isJohnOlder: " + isJohnOlder);
+// // console.log("isJohnEqual: " + isJohnEqual);
 
-// // if (condition)
-// // if (true)
-// // if (false)
-// if(isJohnOlder) //(ageJohn > ageMark) geht auch
 
-// {   //Ja-Zweig / true
-//     console.log("John ist älter");
+// // /******** IF ********/
+// // // TINA --> There is no alternative!
+// // //entweeder JA oder nix ... alternativlos
+
+// // // if (condition)
+// // // if (true)
+// // // if (false)
+// // if(isJohnOlder) //(ageJohn > ageMark) geht auch
+
+// // {   //Ja-Zweig / true
+// //     console.log("John ist älter");
+// // }
+// // else {
+// //     // Nein -Zweig /false
+// //     console.log("John ist jünger. ");
+// // }
+
+// // //******* Ternäre (ternary) schreibweise *******//
+// // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+// /******* Fallunterscheidung / CASE|SWITCH 1 ******/
+
+// let firstName = "Jane";
+// let job;
+// job ="driver"; // ... fährt Taxi!
+// job ="diver";  // ... taucht im Rhein!
+// job ="artist"; // ... malt ein Bild!
+// job ="pilot";  // ... fliegt ein Flugzeug!
+
+// job ="teachter"; // ... unterrichtet!
+// job ="instructor"; // .. unterrichtet!
+
+// switch (job)
+// {
+//     case "driver": //1. Fall (case)
+// console.log(firstName + " fährt Taxi!");
+//     break;
+//     case "diver": //2. Fall (case)
+// console.log(firstName + " taucht im Rhein!");
+//     break;
+//     case "artist": //3. Fall (case)
+// console.log(firstName + " malt ein Bild!");
+//      break;
+//      case "teacher": //4. Fall (case)
+//      case "instructor": //4a. Fall (case)
+//      console.log(firstName + " unterrichtet!");
+//     break;
+
+
+//     default: //// für alle nicht bezeichneten Fälle ...
+//     console.log(firstName + " macht etwas anderes!");
+//     break;
 // }
-// else {
-//     // Nein -Zweig /false
-//     console.log("John ist jünger. ");
-// }
 
-// //******* Ternäre (ternary) schreibweise *******//
-// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+/****** Fallunterscheidung / CASE|SWITCH 2 ******/
 
+const a = 2;
 
-/******* Fallunterscheidung / CASE|SWITCH 1 ******/
-
-let firstName = "Jane";
-let job;
-job ="driver"; // ... fährt Taxi!
-job ="diver";  // ... taucht im Rhein!
-job ="artist"; // ... malt ein Bild!
-job ="pilot";  // ... fliegt ein Flugzeug!
-
-job ="teachter"; // ... unterrichtet!
-job ="instructor"; // .. unterrichtet!
-
-switch (job)
-{
-    case "driver": //1. Fall (case)
-console.log(firstName + " fährt Taxi!");
-    break;
-    case "diver": //2. Fall (case)
-console.log(firstName + " taucht im Rhein!");
-    break;
-    case "artist": //3. Fall (case)
-console.log(firstName + " malt ein Bild!");
-     break;
-     case "teacher": //4. Fall (case)
-     case "instructor": //4a. Fall (case)
-     console.log(firstName + " unterrichtet!");
-    break;
-
-
-    default: //// für alle nicht bezeichneten Fälle ...
-    console.log(firstName + " macht etwas anderes!");
-    break;
+switch(true) {
+    case (a == 1):
+        console.log("a ist 1");
+        break;
+    case (a == 2):
+        console.log("a ist 2");
+        break;
+    default:
+            console.log("a hat einen anderen Wert");
+        break;
 }
