60305da3544d90fefb87e453d70c1d91e914e4b5
Misha <host.misha@googlemail.com>
2021-07-29 12:05:35 +0200

    Equal Older

--- script_01.js
+++ script_01.js
@@ -1,2 +1,18 @@
 // Entscheidungsstrukturen | control structures
 
+//Deklaration
+let ageJohn, ageMark;
+let isJohnOlder, isJohnEqual;
+
+// Assigment (Wertzuweisung)
+ageJohn = 35;
+ageMark = 30;
+
+// Test | Logisch Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+
+// Ausgabe
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
